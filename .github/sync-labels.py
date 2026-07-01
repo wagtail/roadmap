689ce6c357e226e2a76d259b9746bfad9f9aa9ca
Thibaud Colas <thibaudcolas@gmail.com>
2026-07-01 10:09:23 +0100

    Add more labels to the repo

--- .github/sync-labels.py
+++ .github/sync-labels.py
@@ -10,12 +10,25 @@ from pathlib import Path
 labels_path = Path(__file__).parent / "labels.json"
 data = json.loads(labels_path.read_text())
 
+existing_labels = {
+    l["name"]
+    for l in json.loads(
+        subprocess.run(
+            ["gh", "label", "list", "--json", "name", "--limit", "999"],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout
+    )
+}
+
 for label in data["labels"]:
+    action = "create" if label["name"] not in existing_labels else "edit"
     subprocess.run(
         [
             "gh",
             "label",
-            "edit",
+            action,
             label["name"],
             "--color",
             label["color"],
